c4fcae469f8c7e551ad1c7946ff9fb6367928f0a
JuliaCybertek <juliapudovikova@gmail.com>
2021-01-31 11:02:28 -0700

    Julia made change

--- src/test/java/com/trycloud/tests/testCase/practice.java
+++ src/test/java/com/trycloud/tests/testCase/practice.java
@@ -3,5 +3,6 @@ package com.trycloud.tests.testCase;
 public class practice {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("Julia make change");
     }
 }
