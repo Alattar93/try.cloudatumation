3cd90f3b00114b352db726263a7e272747dac4e7
JuliaCybertek <juliapudovikova@gmail.com>
2021-02-06 14:27:49 -0700

    US3TC1

--- src/test/java/com/trycloud/tests/testCase/UserStory3_TC1.java
+++ src/test/java/com/trycloud/tests/testCase/UserStory3_TC1.java
@@ -1,9 +1,12 @@
 package com.trycloud.tests.testCase;
 
 import com.trycloud.tests.base.TestBase;
+import com.trycloud.utilities.ConfigurationReader;
 import com.trycloud.utilities.Driver;
 import com.trycloud.utilities.TrycloudUtil;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -12,9 +15,21 @@ public class UserStory3_TC1 extends TestBase {
 
     @Test
     public void Test1(){
-         TrycloudUtil.login(driver);
-        //.Login as a user2.
-        //Verify the page title is Files module’s tile
+        WebElement inputUsername = driver.findElement(By.name("user"));
+        String username = ConfigurationReader.getProperty("username");
+        inputUsername.sendKeys(username);
+
+        WebElement inputPassword = driver.findElement(By.name("password"));
+        String password = ConfigurationReader.getProperty("password");
+        inputPassword.sendKeys(password);
+
+
+        WebElement loginButton = driver.findElement(By.id("submit-form"));
+        loginButton.click();
+
+
+
+
         String actualTitle=driver.getTitle();
         String excpectedTitle="Dashboard - Trycloud";
         System.out.println(actualTitle);
