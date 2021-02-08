faf81742713bf2ecfc208dec3e180ea764881991
Alattar93 <71522338+Alattar93@users.noreply.github.com>
2021-02-07 20:44:33 -0800

    Update UserStory3_TC2.java

--- src/test/java/com/trycloud/tests/testCase/UserStory3_TC2.java
+++ src/test/java/com/trycloud/tests/testCase/UserStory3_TC2.java
@@ -11,7 +11,7 @@ public class UserStory3_TC2 extends TestBase {
     public void Test_CheckBoxVerification(){
        // Test case #2 - verify users can select all the uploaded files from the page
       //  1.Login as a user
-        TrycloudUtil.login(driver);
+        TrycloudUtil.login();
         // 2.Click the top left checkbox of the table
 
         // 3.Assert all the files are selected
